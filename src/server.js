f9370ad9ec670f0b700af70c32eedfcd8638bff2
Nate Kinne <nathan.kinne@gmail.com>
2019-07-28 18:44:25 -0500

    bug fix

--- src/server.js
+++ src/server.js
@@ -6,8 +6,6 @@ app.get('/', function(req, res){
     res.send('Hello, world!');
 });
 
-// start server
-const PORT = process.env.PORT || 8000
 
 app.listen(PORT, () => {
     console.log(`Server listening at http://localhost:${PORT}`)
